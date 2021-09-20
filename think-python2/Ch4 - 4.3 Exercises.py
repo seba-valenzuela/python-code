5f3aabc3b5fb674c506b9298971ffd444f30abf4
seba-valenzuela <svalenzuelamusic@gmail.com>
2021-09-20 16:59:38 -0400

    09/20/21 update

--- think-python2/Ch4 - 4.3 Exercises.py
+++ think-python2/Ch4 - 4.3 Exercises.py
@@ -4,33 +4,35 @@ seba = turtle.Turtle()
 
 def square(t, length):
     for i in range(4):
-        seba.fd(length)
-        seba.lt(90)
+        t.fd(length)
+        t.lt(90)
 
-def polygon(t, length, n):
+def polyline(t, n, length, angle):
     for i in range(n):
-        seba.fd(length)
-        seba.lt(360/n)
+        t.fd(length)
+        t.lt(angle)
 
-def circle(t, r):
-    circumference =  2 * math.pi * r
-    n = int(circumference / 3) + 3
-    length = circumference / n
-    polygon(t, length, n)
+def polygon(t, n, length):
+    angle = 360 / n
+    polyline(t, n, length, angle)
 
-def polyline(t, r, angle):
+def arc(t, r, angle):
     arc_length =  2 * math.pi * r * angle / 360
     n = int(arc_length / 3) + 1
     step_length = arc_length / n
-    step_angle = angle / n
+    step_angle = float(angle) / n
+    polyline(t, n, step_length, step_angle)
     
-    for i in range(n):
-        t.fd(step_length)
-        t.lt(step_angle)
+    # for i in range(n):
+    #     t.fd(step_length)
+    #     t.lt(step_angle)
+
+def circle(t, r):
+    arc(t, r, 360)
 
 # square(seba, 350)
 
-# polygon(seba, 50, 13)
+# polygon(seba, 13, 50)
 
-# circle(seba, 100)
+circle(seba, 100)
 
