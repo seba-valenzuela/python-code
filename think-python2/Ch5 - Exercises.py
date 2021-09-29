ad33e8ab852460529d131221888e54191d8b1a44
seba-valenzuela <svalenzuelamusic@gmail.com>
2021-09-29 14:00:49 -0400

    09/29/21 update

--- think-python2/Ch5 - Exercises.py
+++ think-python2/Ch5 - Exercises.py
@@ -1,4 +1,7 @@
 import time
+import turtle
+
+print("\n------------------------------------------\n")
 
 # Exercise 5.1 - Display Time
 
@@ -16,11 +19,12 @@ current_sec = (time_now % 60)
 print("The current time is " + str(int(current_hour)) + ":" + str(int(current_min)) + ":" + str(int(current_sec)) + " P.M.")
 print("There have been "+ str(int(days)) +" days since the epoch (Jan 1, 1970)")
 
+print("\n------------------------------------------\n")
+
 # Exercise 5.2 - Check Fermat's Theorem
 # It says that there are no positive integers a, b, and c such that
 # a^n + b^n = c^n, for any values of n greater than 2
 
-print("\n------------------------------------------\n")
 
 def check_fermat (a, b, c, n):
     if (n > 2) and (a^n + b^n == c^n):
@@ -36,8 +40,89 @@ def user_prompt():
     n = input("Provide a value for n: ")
     check_fermat(int(a), int(b), int(c), int(n))
 
-user_prompt()
+# user_prompt()
+
+print("\n------------------------------------------\n")
 
 # Exercise 5.3 - Test for Triangle
 
+def is_triangle(a, b, c):
+    if a > (b+c):
+        print("No.")
+    elif b > (c+a):
+        print("No.")
+    elif c > (a+b):
+        print("No.")
+    else:
+        print("Yes!")
+
+def check_triangle():
+    a = input("Input length of triangle side 1: ")
+    b = input("Input length of triangle side 2: ")
+    c = input("Input length of triangle side 3: ")
+    print("Calculating whether or not these sides can form a triangle... ")
+    is_triangle(int(a), int(b), int(c))
+
+# check_triangle()
+
 print("\n------------------------------------------\n")
+
+# Exercise 5.4 - Draw a Stack Diagram that shows the state of the program when it prints the result:
+
+"""
+def recurse(n, s):
+    if n == 0:
+        print(s)
+    else:
+        recurse(n-1, n+s)
+
+recurse(3, 0)
+"""
+
+# STACK DIAGRAM:
+
+"""
+__main__ : 
+recurse : n = 3, s = 0
+recurse : n = 2, s = 2
+recurse : n = 1, s = 4
+recurse : n = 0, s = 5
+"""
+
+# If you called recurse(-1,0), n would continue to decrease negatively 
+# and eventually display a Runtime error due to recursion
+
+# Docstring (add this inside a function)
+""" In order to use this recurse(n,s) properly, 
+only use positive integers for n """
+
+print("\n------------------------------------------\n")
+
+
+# Exercise 5.5
+
+t = turtle.Turtle()
+
+def koch(t, n):
+    """Draws a koch curve with length n."""
+    if n < 10:
+        t.fd(n)
+        return
+    m = n/3
+    koch(t, m)
+    t.lt(60)
+    koch(t, m)
+    t.rt(120)
+    koch(t, m)
+    t.lt(60)
+    koch(t, m)
+
+def snowflake(t, n):
+    """Draws a snowflake (a triangle with a Koch curve for each side)."""
+    for i in range(3):
+        koch(t, n)
+        t.rt(120)
+
+# koch(t, 100)
+
+snowflake(t, 50)
