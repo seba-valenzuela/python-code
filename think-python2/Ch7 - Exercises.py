d5ec7d5b2dd38d5d2a151dddfed22f58c7cb706a
seba-valenzuela <svalenzuelamusic@gmail.com>
2021-10-27 23:55:46 -0400

    10/27/21 Update

--- think-python2/Ch7 - Exercises.py
+++ think-python2/Ch7 - Exercises.py
@@ -2,22 +2,25 @@
 
 import sys
 
-def start_here():
+def collect_input():
 
     input_data = []
     print()
-    user_input = input("Input an integer to find the square root: ")
+    user_input = input("Input an integer to find the square root, or type 'exit': ")
 
     while user_input != "exit":
         input_data.append(user_input)
-        user_input = input("Input another integer, or type 'exit': ")
+        user_input = input("Input additional integer, or type 'exit': ")
         
 
-    print(input_data) # scaffolding for testing
+    # print(input_data) # scaffolding for testing
 
     # print menu
     print ("{:<4} {:<14} {:<14} {:<14}".format('a','mysqrt(a)','math.sqrt(a)','diff'))
     print ("{:<4} {:<14} {:<14} {:<14}".format('-','---------','------------','----'))
+
+    return input_data
+
 
 # Provide a parameter, "a", and return the square root of "a"
 def my_sqrt(a):
@@ -25,7 +28,8 @@ def my_sqrt(a):
     # data to be printed goes here
     data = []
 
-    x = 0 # dummy variable
+    # The initial guess for the square root will be HALF of 'a' (rounded down)
+    x = int(a/2)
 
     while True:
         print(x)
@@ -34,4 +38,6 @@ def my_sqrt(a):
             break   # if the difference between y and x is less than epsilon, break
         x = y
 
-start_here()
+# collect_input()
+
+my_sqrt(49)
--- think-python2/test.py
+++ think-python2/test.py
@@ -2,6 +2,10 @@ def dummy():
     return 2
 
 def real():
-    return dummy()*2
+    return dummy()*2 # this returns 4
 
 print(real())
+
+x = int(7/2)
+
+print(x)
