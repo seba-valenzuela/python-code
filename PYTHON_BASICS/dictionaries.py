d7dc5a2ac3afd632fe63a3748cd9b22bc1ee3107
seba-valenzuela <svalenzuelamusic@gmail.com>
2022-05-09 13:43:54 -0400

    05/09/22 Update

--- PYTHON_BASICS/dictionaries.py
+++ PYTHON_BASICS/dictionaries.py
@@ -13,3 +13,17 @@ print()
 # Print just the Values:
 print(*data.values()) # <--- The magic asterix! *
 print()
+
+# Lets test the integrity of keys when you remove an item...
+list_of_names = {1:"jon", 2:"seba", 3:"carlos", 4:"jessica"}
+print(list_of_names)
+print()
+
+# delete the 2nd item, which is key 2:
+del list_of_names[2]
+print(list_of_names)
+print()
+
+# try to access item with key 2:
+print(list_of_names.get(2, "This is a custom error message!"))
+print()
